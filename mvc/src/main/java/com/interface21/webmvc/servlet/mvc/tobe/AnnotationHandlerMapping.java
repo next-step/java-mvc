f13cb096a3a72093eead1e69554fb6274fa238e0
jinyoung <jinyoungchoi95@gmail.com>
2024-07-12 00:37:52 +0900

    feat: AnnotationHandlerMapping 패키에서 Request 정보 추출 후 저장기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +26,37 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        try {
+            Map<HandlerKey, HandlerExecution> handlerExecutions = new HashMap<>();
+            for (Object basePackage1 : basePackage) {
+                Reflections reflections = new Reflections(basePackage1);
+                Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+                for (Class<?> controller : controllers) {
+                    Object controllerInstance = controller.getConstructor().newInstance();
+                    List<Method> methods = Arrays.stream(controller.getMethods())
+                            .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                            .toList();
+                    for (Method method : methods) {
+                        RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+                        String url = requestMapping.value();
+                        RequestMethod[] requestMethods = requestMapping.method();
+                        for (RequestMethod requestMethod : requestMethods) {
+                            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                            handlerExecutions.put(handlerKey, new HandlerExecution(controllerInstance, method.getName()));
+                        }
+                    }
+                }
+            }
+            this.handlerExecutions.putAll(handlerExecutions);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        String url = request.getRequestURI();
+        return handlerExecutions.get(new HandlerKey(url, requestMethod));
     }
 }
