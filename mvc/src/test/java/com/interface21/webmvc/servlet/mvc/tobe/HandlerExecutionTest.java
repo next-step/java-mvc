02a1f798f927cca1e253eaf915c248fad311ae6f
jinyoung <jinyoungchoi95@gmail.com>
2024-07-11 20:30:46 +0900

    test: 메소드 파라미터 못찾는 경우 테스트케이스 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionTest.java
@@ -1,6 +1,10 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,8 +17,17 @@ class HandlerExecutionTest {
                 .isInstanceOf(NoSuchMethodException.class);
     }
 
+    @Test
+    void 생성_시_지원하는_파라미터와_불일치하면_예외가_발생한다() {
+        assertThatThrownBy(() -> new HandlerExecution(new HandlerExecutionController(), "handleError"))
+                .isInstanceOf(NoSuchMethodException.class);
+    }
+
     @Controller
     static class HandlerExecutionController {
-
+        @RequestMapping
+        public ModelAndView handleError(HttpServletResponse response, HttpServletRequest request) {
+            return null;
+        }
     }
 }
