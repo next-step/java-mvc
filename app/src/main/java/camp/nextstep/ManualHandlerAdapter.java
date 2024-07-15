80b78a1f4f5d00a199abc8685d8d2b120e66a533
jinyoung <jinyoungchoi95@gmail.com>
2024-07-15 23:28:26 +0900

    refactor: 패키지 이동

--- app/src/main/java/camp/nextstep/ManualHandlerAdapter.java
+++ app/src/main/java/camp/nextstep/ManualHandlerAdapter.java
@@ -2,6 +2,7 @@ package camp.nextstep;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
