0f0c533327803c22ffbd031b3ad91366d7160c02
jinyoung <jinyoungchoi95@gmail.com>
2024-07-15 22:49:19 +0900

    feat: DispatcherServlet에서 ModelAndView 사용하도록 수정

--- app/src/main/java/camp/nextstep/DispatcherServlet.java
+++ app/src/main/java/camp/nextstep/DispatcherServlet.java
@@ -1,5 +1,6 @@
 package camp.nextstep;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.ServletException;
@@ -33,20 +34,11 @@ public class DispatcherServlet extends HttpServlet {
         try {
             final var controller = (Controller) handlerMappingRegistry.getHandler(request);
             final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            final var modelAndView = new ModelAndView(new JspView(viewName));
+            modelAndView.render(request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
-    }
-
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
     }
 }
