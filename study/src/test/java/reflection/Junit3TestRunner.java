739d9293a903f6a7d525f663d8696ba0c83eb15c
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-15 11:39:40 +0900

    test(Junit3TestRunner): Junit3Test에서 test로 시작하는 메소드 실행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,28 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> invoke(instance, method));
+    }
+
+    private void invoke(final Junit3Test instance, final Method method) {
+        try {
+            method.invoke(instance);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
