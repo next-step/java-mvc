38979a20b62ac4bc026839c83b9a7b4cc45fa39d
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-17 00:36:01 +0900

    fix(DispatcherServlet):  ManualHandlerMapping, AnnotationHandlerMapping 2개를 List 로 관리하도록 변경

--- app/src/main/java/camp/nextstep/DispatcherServlet.java
+++ app/src/main/java/camp/nextstep/DispatcherServlet.java
@@ -1,27 +1,41 @@
 package camp.nextstep;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.webmvc.servlet.view.JspView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final List<HandlerMapping> handlerMappings;
 
     public DispatcherServlet() {
+        this.handlerMappings = new ArrayList<>();
     }
 
     @Override
     public void init() {
-        this.manualHandlerMapping = new ManualHandlerMapping();
-        this.manualHandlerMapping.initialize();
+        final ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
+        manualHandlerMapping.initialize();
+        final AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping("samples");
+        annotationHandlerMapping.initialize();
+
+        handlerMappings.add(manualHandlerMapping);
+        handlerMappings.add(annotationHandlerMapping);
     }
 
     @Override
@@ -29,23 +43,35 @@ public class DispatcherServlet extends HttpServlet {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
+        final Object handler = getHandler(request);
+
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
-        } catch (Throwable e) {
+            final ModelAndView modelAndView = execute(handler, request, response);
+            final View view = modelAndView.getView();
+            view.render(modelAndView.getModel(), request, response);
+        } catch (final Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
+    private ModelAndView execute(final Object handler, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        if (handler instanceof final Controller controller) {
+            return controller.execute(request, response);
         }
+        if (handler instanceof final HandlerExecution handlerExecution) {
+            return handlerExecution.handle(request, response);
+        }
+        throw new ServletException("Unknown handler type: " + handler.getClass().getName());
+    }
 
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
+    private Object getHandler(final HttpServletRequest request) throws ServletException {
+        for (final HandlerMapping handlerMapping : handlerMappings) {
+            final Object handler = handlerMapping.getHandler(request);
+            if (handler != null) {
+                return handler;
+            }
+        }
+        throw new ServletException("No handler found for request URI: " + request.getRequestURI());
     }
 }
