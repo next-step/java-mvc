2415ab77f37b1cf2f9addb9175f34c39ff767372
jinyoung <jinyoungchoi95@gmail.com>
2024-07-17 00:28:48 +0900

    test: 테스트 메소드 수정

--- app/src/test/java/camp/nextstep/controller/UserControllerTest.java
+++ app/src/test/java/camp/nextstep/controller/UserControllerTest.java
@@ -19,22 +19,17 @@ import static org.mockito.Mockito.when;
 class UserControllerTest {
 
     private final UserController userController = new UserController();
+    private final HttpServletRequest request = mock(HttpServletRequest.class);
+    private final HttpServletResponse response = mock(HttpServletResponse.class);
+    private final HttpSession session = mock(HttpSession.class);
 
     @Test
     void 현재_로그인된_유저의_정보를_반환한다() {
-        // given
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
         User givenUser = new User(1L, "jinyoung", "password", "jinyoungchoi95@gmail.com");
-
         when(request.getSession()).thenReturn(session);
         when(session.getAttribute("user")).thenReturn(givenUser);
 
-        // when
         ModelAndView actual = userController.show(request, response);
-
-        // then
         assertAll(
                 () -> assertThat(actual.getView()).isInstanceOf(JsonView.class),
                 () -> assertThat(actual.getModel()).containsExactly(Map.entry("user", givenUser))
@@ -43,16 +38,9 @@ class UserControllerTest {
 
     @Test
     void 현재_로그인되지_않았다면_401을_리다이랙한다() {
-        // given
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
-
-        // when
         when(request.getSession()).thenReturn(session);
         when(session.getAttribute("user")).thenReturn(null);
 
-        // then
         ModelAndView actual = userController.show(request, response);
         assertThat(actual.getView()).isEqualTo(new JspView("redirect:/401.jsp"));
     }
