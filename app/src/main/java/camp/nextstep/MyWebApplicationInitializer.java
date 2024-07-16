385fc94c62a60e8898669e26750de648afab66c1
jinyoung <jinyoungchoi95@gmail.com>
2024-07-17 00:07:53 +0900

    feat: application root package 수정

--- app/src/main/java/camp/nextstep/MyWebApplicationInitializer.java
+++ app/src/main/java/camp/nextstep/MyWebApplicationInitializer.java
@@ -18,7 +18,7 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(final ServletContext servletContext) {
-        final var dispatcherServlet = new DispatcherServlet("camp");
+        final var dispatcherServlet = new DispatcherServlet(this.getClass().getPackageName());
 
         final var registration = servletContext.addServlet(DEFAULT_SERVLET_NAME, dispatcherServlet);
         if (registration == null) {
