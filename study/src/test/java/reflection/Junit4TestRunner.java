465f3d58a58d5923ada163f22dd2081fd6120edb
woo-yu <hjwoou@gmail.com>
2024-07-16 20:57:35 +0900

    [학습테스트] Reflection API

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,17 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(new Junit4Test());
+            }
+        }
     }
 }
