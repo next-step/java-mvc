152d74c8bd67a1c50989aee7789699b4ecd7d15d
문주현 <hoopster@tmapmobility.com>
2024-07-16 23:44:13 +0900

    feature : add initialize, getHandler method logic

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +28,53 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> handlerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+
+        Map<HandlerKey, HandlerExecution> map = handlerClasses.stream()
+                        .map(controller -> {
+                            try {
+                                return controllerToHandlerExecutions(controller);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }).flatMap(map1 -> map1.entrySet().stream())
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        handlerExecutions.putAll(map);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private Map<HandlerKey, HandlerExecution> controllerToHandlerExecutions(final Class<?> controller) throws Exception {
+        Object instance = controller.getDeclaredConstructor()
+                .newInstance();
+
+        return Arrays.stream(controller.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .map(method -> {
+                    final RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+                    final String path = requestMapping.value();
+                    final RequestMethod[] methodTypes = requestMapping.method();
+                    final HandlerExecution handlerExecution;
+                    try {
+                        handlerExecution = new HandlerExecution(instance, method.getName());
+                    } catch (NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    return Arrays.stream(methodTypes)
+                            .map(methodType -> new HandlerKey(path, methodType))
+                            .collect(Collectors.toMap(
+                                    handlerKey -> handlerKey,
+                                    handlerKey -> handlerExecution
+                                    )
+                            );
+                }).flatMap(map -> map.entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
