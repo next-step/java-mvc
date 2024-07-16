0bbe5dbcd7b0102be6606736bbe69a15f64d3c2a
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-16 23:27:27 +0900

    fix(HandlerMapping): 인터페이스 추출
    - AnnotationHandlerMapping 과 ManualHandlerMapping 가 해당 인터페이스를 구현하도록 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
