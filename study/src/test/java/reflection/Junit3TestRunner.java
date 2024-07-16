61d4af94196e9c654422f05a75a89d4dd9c0b8ab
문주현 <hoopster@tmapmobility.com>
2024-07-16 17:07:12 +0900

    test : refactor Reflection API test

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,8 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +11,16 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getClassLoader()
+                .loadClass(clazz.getName())
+                .getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.getDeclaredConstructor().newInstance());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,6 +2,8 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class Junit4TestRunner {
 
     @Test
@@ -9,5 +11,16 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getClassLoader()
+                .loadClass(clazz.getName())
+                .getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.getDeclaredConstructor().newInstance());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
