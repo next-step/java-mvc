b6d6311156957c70a8d187879dd054d3d04208fe
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-07-17 00:25:23 +0900

    test : 리플렉션 학습테스트

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,26 @@
 package reflection;
 
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
+    @DisplayName("Junit4Test 클래스의 메서드 중 @MyTest 애노테이션이 설정된 메서드를 찾아 실행한다.")
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test test = clazz.getDeclaredConstructor().newInstance();
+
+        Stream.of(clazz.getDeclaredMethods())
+            .filter(method -> method.isAnnotationPresent(MyTest.class))
+            .forEach(method -> {
+                try {
+                    method.invoke(test);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
     }
 }
