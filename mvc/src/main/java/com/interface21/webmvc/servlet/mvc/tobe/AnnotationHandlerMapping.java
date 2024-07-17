25376e00be18780266e9df489e1a8fa58e0e166c
woo-yu <59992230+woo-yu@users.noreply.github.com>
2024-07-17 11:50:48 +0900

    🚀 1단계 - @MVC 구현하기 (#8)
    * [학습테스트] Reflection API
    
    * [feat] AnnotationHandlerMappingTest 테스트 통과
    
    * [feat] JspView 구현
    
    * [fix] class 보다는 controller 라는 정확한 명칭으로 변경
    
    * [fix] MethodHandlerExecution -> RequestMappedHandlerExecution 명칭 변경
    
    * [fix] request로 HandlerKey 생성자 추가
    
    * [fix] mapping -> handler 명칭 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,9 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,9 +28,32 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        var controllers = new Reflections(basePackage).getTypesAnnotatedWith(Controller.class);
+        controllers.forEach(this::mappingController);
+    }
+
+    private void mappingController(Class<?> controller) {
+        try {
+            var controllerInstance = controller.getDeclaredConstructor().newInstance();
+            Arrays.stream(controller.getMethods()).forEach(method -> mappingHandler(controllerInstance, method));
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException ignored) {
+        }
+    }
+
+    private void mappingHandler(final Object controllerInstance, final Method method) {
+        var handler = method.getAnnotation(RequestMapping.class);
+        if (handler == null) {
+            return;
+        }
+        for (RequestMethod requestMethod : handler.method()) {
+            var key = new HandlerKey(handler.value(), requestMethod);
+            var execution = new RequestMappedHandlerExecution(controllerInstance, method);
+            handlerExecutions.put(key, execution);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        return handlerExecutions.get(new HandlerKey(request));
     }
 }
--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,18 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run() throws InvocationTargetException, IllegalAccessException {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(new Junit3Test());
+            }
+        }
     }
 }
