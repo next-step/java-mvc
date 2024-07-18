b7e088ec4b528cca3cc0e3abd76b2299d7681d4b
SeolYoungKim <nasur4da@gmail.com>
2024-07-18 23:50:05 +0900

    refactor(Controller): ModelAndView를 반환하도록 변경

--- app/src/main/java/camp/nextstep/controller/RegisterController.java
+++ app/src/main/java/camp/nextstep/controller/RegisterController.java
@@ -2,6 +2,8 @@ package camp.nextstep.controller;
 
 import camp.nextstep.domain.User;
 import camp.nextstep.dao.InMemoryUserDao;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
@@ -9,13 +11,13 @@ import com.interface21.webmvc.servlet.mvc.asis.Controller;
 public class RegisterController implements Controller {
 
     @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
                 req.getParameter("email"));
         InMemoryUserDao.save(user);
 
-        return "redirect:/index.jsp";
+        return new ModelAndView(new JspView("redirect:/index.jsp"));
     }
 }
