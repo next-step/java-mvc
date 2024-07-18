712528a91da6b353acca3a94996b894cdcc94bb3
Kim Hyuntae <75936123+Hyunta@users.noreply.github.com>
2024-07-18 21:52:21 +0900

    🚀 1단계 - @MVC 구현하기 (#9)
    * 학습 테스트 진행
    
    * feature: HandlerExecution 구현
    
    * feature: AnnotationHandlerMapping 구현
    
    * feature: 하나의 경로에 HttpMethod가 여러개일 때 처리하도록 수정
    
    * feature: HandlerExecution의 값을 미리 인스턴스화하도록 수정
    
    * feature: JspView 구현
    
    * docs: README.md 작성

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +28,50 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Set<Class<?>> controllers = new Reflections(basePackage).getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controller : controllers) {
+            registerHandlerMappings(controller);
+        }
+    }
+
+    private void registerHandlerMappings(Class<?> controller) {
+        for (Method method : controller.getDeclaredMethods()) {
+            registerHandlerMapping(controller, method);
+        }
+    }
+
+    private void registerHandlerMapping(Class<?> controller, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping != null) {
+            List<HandlerKey> handlerKeys = makeHandlerKeys(requestMapping);
+            HandlerExecution handlerExecution = new HandlerExecution(instantiate(controller), method);
+            for (HandlerKey handlerKey : handlerKeys) {
+                handlerExecutions.put(handlerKey, handlerExecution);
+                log.info("Mapped {} to {}", handlerKey, handlerExecution);
+            }
+        }
+    }
+
+    private Object instantiate(Class<?> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<HandlerKey> makeHandlerKeys(RequestMapping requestMapping) {
+        String url = requestMapping.value();
+        RequestMethod[] methods = requestMapping.method();
+        return Arrays.stream(methods)
+                     .map(method -> new HandlerKey(url, method))
+                     .toList();
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
