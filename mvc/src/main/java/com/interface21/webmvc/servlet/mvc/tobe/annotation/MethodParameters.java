5725e02efd196e1789c0fad220426969c27a8c18
jinyoung <jinyoungchoi95@gmail.com>
2024-07-19 00:17:04 +0900

    feat: MethodParameters에서 request를 받아 모든 파싱값 반환 추가 및 테스트

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParameters.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParameters.java
@@ -1,5 +1,7 @@
 package com.interface21.webmvc.servlet.mvc.tobe.annotation;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +17,12 @@ public class MethodParameters {
     private List<MethodParameter> parseMethodParameters(Method method) {
         return Arrays.stream(method.getParameters())
                 .map(MethodParameter::new)
+                .toList();
+    }
+
+    public List<Object> parseValues(HttpServletRequest request) {
+        return values.stream()
+                .map(parameter -> parameter.parseValue(request))
                 .toList();
     }
 
--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParametersTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParametersTest.java
@@ -1,20 +1,27 @@
 package com.interface21.webmvc.servlet.mvc.tobe.annotation;
 
 import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class MethodParametersTest {
 
-    @Test
-    void 메소드를_받아_파라미터리스트를_생성한다() throws NoSuchMethodException {
-        Method method = TestUserController.class
-                .getMethod("createString", String.class, int.class);
+    private final Method method = TestUserController.class
+            .getMethod("createString", String.class, int.class);
 
+    MethodParametersTest() throws NoSuchMethodException {
+    }
+
+    @Test
+    void 메소드를_받아_파라미터리스트를_생성한다() {
         MethodParameters actual = new MethodParameters(method);
         assertAll(
                 () -> assertThat(actual.getValues()).hasSize(2),
@@ -23,6 +30,17 @@ class MethodParametersTest {
                         new MethodParameter(int.class, "userAge")
                 )
         );
+    }
+
+    @Test
+    void request에서_파싱한_값을_반환한다() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getAttribute("userId")).thenReturn("jinyoung");
+        when(request.getAttribute("userAge")).thenReturn(10);
+
+        MethodParameters methodParameters = new MethodParameters(method);
+        List<Object> actual = methodParameters.parseValues(request);
+        assertThat(actual).containsExactly("jinyoung", 10);
     }
 
     private static class TestUserController {
