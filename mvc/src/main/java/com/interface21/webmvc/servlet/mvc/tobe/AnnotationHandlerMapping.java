0a0293f16daefa11258353489550b8aca3d521d4
Hyunta <mohai2618@gmail.com>
2024-07-18 20:52:06 +0900

    feature: AnnotationHandlerMapping 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +26,44 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Set<Class<?>> controllers = findControllerClasses();
+        for (Class<?> controller : controllers) {
+            registerHandlerMappings(controller);
+        }
+    }
+
+    private Set<Class<?>> findControllerClasses() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void registerHandlerMappings(Class<?> controller) {
+        Method[] methods = controller.getDeclaredMethods();
+        for (Method method : methods) {
+            registerHandlerMapping(controller, method);
+        }
+    }
+
+    private void registerHandlerMapping(Class<?> controller, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping != null) {
+            HandlerKey handlerKey = makeHandlerKey(requestMapping);
+            HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+            log.info("Mapped {} to {}", handlerKey, handlerExecution);
+        }
+    }
+
+    private HandlerKey makeHandlerKey(RequestMapping requestMapping) {
+        String url = requestMapping.value();
+        RequestMethod requestMethod = requestMapping.method()[0];
+        return new HandlerKey(url, requestMethod);
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
