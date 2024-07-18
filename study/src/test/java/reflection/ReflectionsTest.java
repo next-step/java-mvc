5ffc92a829936913eda271e3b7bb8716626f8e8e
SeolYoungKim <nasur4da@gmail.com>
2024-07-18 21:58:04 +0900

    study(Reflection): 추가로 학습한 내용 및 참고 자료 정리

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -10,13 +10,16 @@ import reflection.annotation.Controller;
 import reflection.annotation.Repository;
 import reflection.annotation.Service;
 
+/**
+ * <a href="https://www.baeldung.com/reflections-library">Reflections</a>
+ */
 class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
     void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
+        Reflections reflections = new Reflections("reflection.examples");  // basePackage로 활용
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
         Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
