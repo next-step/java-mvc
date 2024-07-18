4581d2f81e51aff5703b5de486664424798bb7fa
jinyoung <jinyoungchoi95@gmail.com>
2024-07-18 23:40:03 +0900

    feat: request에서 primitive 반환 추가 및 테스트

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParameter.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParameter.java
@@ -1,5 +1,7 @@
 package com.interface21.webmvc.servlet.mvc.tobe.annotation;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.lang.reflect.Parameter;
 
 public class MethodParameter {
@@ -7,9 +9,20 @@ public class MethodParameter {
     private final Class<?> parameterType;
     private final String parameterName;
 
+    public MethodParameter(Class<?> parameterType, String parameterName) {
+        this.parameterType = parameterType;
+        this.parameterName = parameterName;
+    }
+
     public MethodParameter(Parameter parameter) {
-        this.parameterType = parameter.getType();
-        this.parameterName = parameter.getName();
+        this(parameter.getType(), parameter.getName());
+    }
+
+    public Object parseValue(HttpServletRequest request) {
+        if (parameterType.isPrimitive()) {
+            return request.getAttribute(parameterName);
+        }
+        throw new IllegalStateException("실행 불가");
     }
 
     public Class<?> getParameterType() {
--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParameterTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/MethodParameterTest.java
@@ -1,12 +1,15 @@
 package com.interface21.webmvc.servlet.mvc.tobe.annotation;
 
 import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Parameter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class MethodParameterTest {
 
@@ -19,6 +22,18 @@ class MethodParameterTest {
         assertAll(
                 () -> assertThat(actual.getParameterType()).isEqualTo(String.class),
                 () -> assertThat(actual.getParameterName()).isEqualTo("userId")
+        );
+    }
+
+    @Test
+    void HttpServletRequest에서_parameterType에_해당하는_값을_파싱한다() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getAttribute("userAge")).thenReturn(10);
+
+        Object actual = new MethodParameter(int.class, "userAge").parseValue(request);
+        assertAll(
+                () -> assertThat(actual.getClass()).isEqualTo(Integer.class),
+                () -> assertThat(actual).isEqualTo(10)
         );
     }
 
