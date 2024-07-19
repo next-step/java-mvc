275305c92042f950ec0b52311361fe4e6b7dac8f
jinyoung <jinyoungchoi95@gmail.com>
2024-07-20 06:53:15 +0900

    feat: HttpServletResponse parser 추가 및 테스트

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/parameter/HttpServletResponseParameterParserTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/annotation/parameter/HttpServletResponseParameterParserTest.java
@@ -7,27 +7,27 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
-class HttpServletRequestParameterParserTest {
+class HttpServletResponseParameterParserTest {
 
     @Test
     void 지원가능한_타입이면_true를_반환한다() {
-        boolean actual = new HttpServletRequestParameterParser(HttpServletRequest.class).accept();
+        boolean actual = new HttpServletResponseParameterParser(HttpServletResponse.class).accept();
         assertThat(actual).isTrue();
     }
 
     @Test
     void 지원불가능한_타입이면_false를_반환한다() {
-        boolean actual = new HttpServletRequestParameterParser(HttpServletResponse.class).accept();
+        boolean actual = new HttpServletResponseParameterParser(HttpServletRequest.class).accept();
         assertThat(actual).isFalse();
     }
 
     @Test
-    void 파싱요청은_request를_그대로_반환한다() {
+    void 파싱요청은_response를_그대로_반환한다() {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpServletRequestParameterParser parser = new HttpServletRequestParameterParser(HttpServletRequest.class);
+        HttpServletResponseParameterParser parser = new HttpServletResponseParameterParser(HttpServletResponse.class);
 
         Object actual = parser.parseValue(request, response);
-        assertThat(actual).isEqualTo(request);
+        assertThat(actual).isEqualTo(response);
     }
 }
