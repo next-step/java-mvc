377e62b3d53d9b23961c854c61b34d40df1bc063
newvelop <newvelop@gmail.com>
2024-07-20 22:22:39 +0900

    feat : requestmapping 호출 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,18 @@ import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
+    private Method method;
+    private Object object;
+
+    public HandlerExecution(Method method, Object object) {
+        this.method = method;
+        this.object = object;
+    }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(object, request, response);
     }
 }
