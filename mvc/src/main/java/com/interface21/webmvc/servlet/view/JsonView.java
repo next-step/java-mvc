9b88fd0df30503a7725b0e5ba17878aa798ae7e4
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-21 13:36:32 +0900

    fix(JsonView): 피드백 반영

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,27 +1,22 @@
 package com.interface21.webmvc.servlet.view;
 
 import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.exception.MessageConverterException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.List;
 import java.util.Map;
 
 public class JsonView implements View {
-    private final List<ModelWriter> modelWriters;
-
-    public JsonView() {
-        modelWriters = List.of(new StringModelWriter(), new ObjectModelWriter());
-    }
+    private final StringModelWriter stringModelWriter = new StringModelWriter();
+    private final ObjectModelWriter objectModelWriter = new ObjectModelWriter();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        final ModelWriter converter = modelWriters.stream()
-                .filter(modelWriter -> modelWriter.support(model))
-                .findFirst()
-                .orElseThrow(() -> new MessageConverterException("Proper MessageConverter is not registered"));
+        if (stringModelWriter.support(model)) {
+            stringModelWriter.write(model, response);
+            return;
+        }
 
-        converter.write(model, response);
+        objectModelWriter.write(model, response);
     }
 }
