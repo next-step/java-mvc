0d90dbc7c6846fa5e8323ef5819853eb4c37eecc
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-07-21 16:35:59 +0900

    feat : ControllerScanner를 통해 controller 수집 및 생성 책임 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,26 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javassist.tools.reflect.Reflection;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
@@ -32,30 +25,30 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
+    @Override
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
-        Reflections reflections = new Reflections(basePackage);
-        reflections.getTypesAnnotatedWith(Controller.class)
-                   .forEach(this::processController);
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        controllerScanner.scan().forEach(this::processController);
     }
 
+    @Override
     public Object getHandler(final HttpServletRequest request) {
-        return handlerExecutions.get(HandlerKey.of(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
+        return handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
     }
 
-    private void processController(Class<?> controller) {
-        Object instance = createInstance(controller);
+    private void processController(Class<?> controller, ControllerInstance instance) {
         Arrays.stream(controller.getMethods())
-              .filter(this::isRequestMappingMethod)
+              .filter(this::hasRequestMappingMethod)
               .flatMap(method -> createHandlerExecutions(instance, method))
               .forEach(this::putHandlerExecution);
     }
 
-    private boolean isRequestMappingMethod(Method method) {
+    private boolean hasRequestMappingMethod(Method method) {
         return method.isAnnotationPresent(RequestMapping.class);
     }
 
-    private Stream<Entry<HandlerKey, HandlerExecution>> createHandlerExecutions(Object instance, Method method) {
+    private Stream<Entry<HandlerKey, HandlerExecution>> createHandlerExecutions(ControllerInstance instance, Method method) {
         HandlerExecution handlerExecution = makeHandlerExecution(instance, method);
         return makeHandlerKeys(handlerExecution).entrySet().stream();
     }
@@ -64,23 +57,15 @@ public class AnnotationHandlerMapping {
         handlerExecutions.put(entry.getKey(), entry.getValue());
     }
 
-    private Object createInstance(Class<?> controller) {
-        try {
-            return controller.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private HandlerExecution makeHandlerExecution(Object controller, Method method) {
-        return new HandlerExecution(controller, method);
+    private HandlerExecution makeHandlerExecution(ControllerInstance instance, Method method) {
+        return new HandlerExecution(instance.getController(), method);
     }
 
     private Map<HandlerKey, HandlerExecution> makeHandlerKeys(final HandlerExecution handlerExecution) {
         RequestMapping requestMapping = handlerExecution.getMethod().getAnnotation(RequestMapping.class);
         return Stream.of(requestMapping.method())
                      .collect(Collectors.toMap(
-                         requestMethod -> HandlerKey.of(requestMapping.value(), requestMethod),
+                         requestMethod -> new HandlerKey(requestMapping.value(), requestMethod),
                          requestMethod -> handlerExecution));
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
@@ -1,6 +1,7 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.context.stereotype.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.ControllerInstance;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,27 +11,13 @@ public class ControllerScanner {
 
     private final Reflections reflections;
 
-    public ControllerScanner(String... basePackage) {
+    public ControllerScanner(Object... basePackage) {
         this.reflections = new Reflections(basePackage);
     }
 
-    public Map<Class<?>, Object> scan() {
+    public Map<Class<?>, ControllerInstance> scan() {
         return reflections.getTypesAnnotatedWith(Controller.class)
                           .stream()
-                          .collect(Collectors.toMap(classes -> classes, this::createInstance));
-    }
-
-    private Object createInstance(Class<?> controller) {
-        try {
-            return controller.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException e) {
-            throw new RuntimeException(controller.getName() + "의 인스턴스화에 실패했습니다.", e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(controller.getName() + "의 생성자에 대한 접근이 허용되지 않습니다.", e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(controller.getName() + "의 생성자 호출 중 예외가 발생했습니다.", e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(controller.getName() + "에 기본 생성자가 없습니다.", e);
-        }
+                          .collect(Collectors.toMap(clazz -> clazz, ControllerInstance::new));
     }
 }
