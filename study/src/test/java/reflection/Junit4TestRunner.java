8c2d35f4872888ae9c6584aadc0b05f23c791fc3
gutenLee <3bnm1128@naver.com>
2024-07-22 23:52:53 +0900

    test: 학습 테스트 작성

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,6 +1,9 @@
 package reflection;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 class Junit4TestRunner {
 
@@ -9,5 +12,15 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        Assertions.assertThatNoException()
+                            .isThrownBy(() -> method.invoke(clazz.getDeclaredConstructor().newInstance()));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
