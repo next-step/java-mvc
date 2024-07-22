b1593021e1a4484a08e3eb64edaabcc4d9087250
문주현 <hoopster@tmapmobility.com>
2024-07-22 23:18:39 +0900

    refactor : ManualHandlerAdapter handle return ModelAndView

--- app/src/main/java/camp/nextstep/ManualHandlerAdapter.java
+++ app/src/main/java/camp/nextstep/ManualHandlerAdapter.java
@@ -3,7 +3,6 @@ package camp.nextstep;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
-import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -17,10 +16,9 @@ public class ManualHandlerAdapter implements HandlerAdapter {
     @Override
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (supports(handler)) {
-            final String view = ((Controller) handler).execute(request, response);
-            return new ModelAndView(new JspView(view));
+            return ((Controller) handler).execute(request, response);
         }
-        
+
         throw new IllegalStateException("This handler is not supported by ManualHandlerAdapter");
     }
 }
