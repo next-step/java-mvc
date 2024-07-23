0969419a07025838ae12dbd04ceee770afd43cb7
junseo <djawnstj44@naver.com>
2024-07-23 23:23:51 +0900

    refactor: 1단계 피드백 반영

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKeys.java
@@ -0,0 +1,24 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class HandlerKeys {
+    private final List<HandlerKey> handlerKeys = new ArrayList<>();
+
+    public void addMethods(final String url, final RequestMethod[] requestMethods) {
+        final List<HandlerKey> keyList = Arrays.stream(requestMethods)
+                .map(method -> new HandlerKey(url, method))
+                .toList();
+
+        this.handlerKeys.addAll(keyList);
+    }
+
+    public boolean hasMatchingMethod(final RequestMethod method) {
+        return this.handlerKeys.stream().anyMatch(key -> key.isMatchMethod(method));
+    }
+}
