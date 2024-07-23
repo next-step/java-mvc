0125ce157542a4077ba657f3d4d86e891b390579
newvelop <newvelop@gmail.com>
2024-07-23 22:33:24 +0900

    test : 핸들러 어댑터 테스트 추가

---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdaptorTest.java
@@ -0,0 +1,38 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.webmvc.servlet.mvc.tobe.exception.NoExactHandlerException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HandlerAdaptorTest {
+    @Test
+    void 매핑된_요청이_아닐경우_예외를_발생시킨다() {
+        // given
+        var annotationHandlerMapping = new AnnotationHandlerMapping(
+                "samples"
+        );
+        annotationHandlerMapping.initialize();
+        HandlerAdaptor handlerAdaptor = new HandlerAdaptor(List.of(
+                annotationHandlerMapping
+        ));
+
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getRequestURI()).thenReturn("/notfound");
+        when(request.getMethod()).thenReturn("GET");
+
+        // when & then
+        assertThrows(
+                NoExactHandlerException.class,
+                () -> handlerAdaptor.execute(request, response)
+        );
+    }
+}
