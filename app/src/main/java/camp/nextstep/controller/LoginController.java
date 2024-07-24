25b53393895b02c1a3be05120f5dbe7e3d14a24f
Sangmin <sm9171@nate.com>
2024-07-24 10:14:49 +0900

    refactor: Annotation으로 컨트롤러 구현하기

--- app/src/main/java/camp/nextstep/controller/LoginController.java
+++ app/src/main/java/camp/nextstep/controller/LoginController.java
@@ -2,19 +2,22 @@ package camp.nextstep.controller;
 
 import camp.nextstep.domain.User;
 import camp.nextstep.dao.InMemoryUserDao;
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @Override
-    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView doLogin(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         if (UserSession.isLoggedIn(req.getSession())) {
             return ModelAndView.ofJspView("redirect:/index.jsp");
         }
@@ -26,6 +29,20 @@ public class LoginController implements Controller {
 
         log.info("User : {}", user);
         return ModelAndView.ofJspView(login(req, user));
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView viewLogin(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return ModelAndView.ofJspView(getPath(req));
+    }
+
+    private static String getPath(HttpServletRequest req) {
+        return UserSession.getUserFrom(req.getSession())
+                .map(user -> {
+                    log.info("logged in {}", user.getAccount());
+                    return "redirect:/index.jsp";
+                })
+                .orElse("/login.jsp");
     }
 
     private String login(final HttpServletRequest request, final User user) {
--- app/src/main/java/camp/nextstep/controller/LogoutController.java
+++ app/src/main/java/camp/nextstep/controller/LogoutController.java
@@ -1,13 +1,16 @@
 package camp.nextstep.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 
-public class LogoutController implements Controller {
+@Controller
+public class LogoutController {
 
-    @Override
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
