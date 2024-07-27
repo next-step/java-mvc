8682998a620f2e2ab010eae83afdb88890a166b9
Hyunta <mohai2618@gmail.com>
2024-07-27 19:27:25 +0900

    refactor: LoginController를 애노테이션 기반의 컨트롤러로 리팩터링

--- app/src/main/java/camp/nextstep/ManualHandlerMapping.java
+++ app/src/main/java/camp/nextstep/ManualHandlerMapping.java
@@ -1,6 +1,5 @@
 package camp.nextstep;
 
-import camp.nextstep.controller.LoginViewController;
 import camp.nextstep.controller.LogoutController;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.asis.ForwardController;
@@ -20,8 +19,6 @@ public class ManualHandlerMapping implements HandlerMapping {
     @Override
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/login", new LoginController());
-        controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
 
         log.info("Initialized Handler Mapping!");
