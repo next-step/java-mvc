acfd296d4a8537d5ce1496cdb0c5a60d027c1a75
Sangmin <sm9171@nate.com>
2024-07-27 14:28:14 +0900

    feat: mainController 추가

---
+++ app/src/main/java/camp/nextstep/controller/MainController.java
@@ -0,0 +1,16 @@
+package camp.nextstep.controller;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return ModelAndView.of("redirect:/index.jsp");
+    }
+}
