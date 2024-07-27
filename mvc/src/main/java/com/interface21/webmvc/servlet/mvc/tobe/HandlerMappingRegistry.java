1a16496dccd3775715f1180f904c5be1e3022ba6
youngbinary <youngbin@barogo.com>
2024-07-27 16:57:05 +0900

    feat: HandlerMapping 리스트를 가지고 있는 Registry 클래스를 추가한다

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerMappingRegistry.java
@@ -0,0 +1,25 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HandlerMappingRegistry {
+    private final List<HandlerMapping> handlerMappings;
+
+    public HandlerMappingRegistry(final HandlerMapping... handlerMappings) {
+        this.handlerMappings = Arrays.asList(handlerMappings);
+    }
+
+    public void initialize() {
+        handlerMappings.forEach(HandlerMapping::initialize);
+    }
+
+    public HandlerMapping getHandlerMapping(final HttpServletRequest request) {
+        return handlerMappings.stream()
+                .filter(handlerMapping -> handlerMapping.getHandler(request).isPresent())
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Unsupported request: " + request.getRequestURI()));
+    }
+}
