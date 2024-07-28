09f9b64e8c9d661a20140687a817426b1ce14954
SeolYoungKim <nasur4da@gmail.com>
2024-07-28 22:20:44 +0900

    refactor(QueryStringParser): 메서드 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/support/QueryStringParser.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/support/QueryStringParser.java
@@ -10,16 +10,27 @@ public class QueryStringParser {
     private static final String KEY_VALUE_SEPARATOR = "=";
 
     public Map<String, String> parse(String queryString) {
-        return Arrays.stream(queryString.split(QUERY_SEPARATOR))
-                .map(query -> query.split(KEY_VALUE_SEPARATOR))
-                .collect(Collectors.toUnmodifiableMap(
-                        keyValue -> keyValue[0],
-                        keyValue -> {
-                            if (keyValue.length == 1) {
-                                return "";
-                            }
-                            return keyValue[1];
-                        }
-                ));
+        return Arrays.stream(splitQueryString(queryString))
+                .map(this::splitKeyAndValue)
+                .collect(Collectors.toUnmodifiableMap(this::getKey, this::getValue));
+    }
+
+    private String[] splitQueryString(String queryString) {
+        return queryString.split(QUERY_SEPARATOR);
+    }
+
+    private String[] splitKeyAndValue(String query) {
+        return query.split(KEY_VALUE_SEPARATOR);
+    }
+
+    private String getKey(String[] keyValue) {
+        return keyValue[0];
+    }
+
+    private String getValue(String[] keyValue) {
+        if (keyValue.length == 1) {
+            return "";
+        }
+        return keyValue[1];
     }
 }
