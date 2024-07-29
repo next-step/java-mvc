d92cf2d195675961c0daa834cd27d73c3d6c4274
qkr2435 <qkr2435@gmail.com>
2024-07-30 02:22:11 +0900

    feat: 요구사항 1 annotation handler 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,29 +1,76 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class AnnotationHandlerMapping {
-
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final String[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(String... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        log.info("Initializing AnnotationHandlerMapping");
+        Set<Class<?>> controllerClasses = scanControllers();
+        for (Class<?> controllerClass : controllerClasses) {
+            Object controllerInstance = createControllerInstance(controllerClass);
+            mapHandlers(controllerClass, controllerInstance);
+        }
+        log.info("AnnotationHandlerMapping initialized {} 개", handlerExecutions.size());
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    public Object getHandler(HttpServletRequest request) {
+        HandlerKey key = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(key);
+    }
+
+    private Set<Class<?>> scanControllers() {
+        Reflections reflections = new Reflections((Object[]) basePackages);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private Object createControllerInstance(Class<?> controllerClass) {
+        try {
+            return controllerClass.getDeclaredConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("인스턴스 생성에 실패했습니다.", e);
+        }
+    }
+
+    private void mapHandlers(Class<?> controllerClass, Object controllerInstance) {
+        String baseUrl = extractBaseUrl(controllerClass);
+        for (Method method : controllerClass.getMethods()) {
+            RequestMapping methodMapping = method.getAnnotation(RequestMapping.class);
+            if (methodMapping != null) {
+                String fullUrl = baseUrl + methodMapping.value();
+                RequestMethod[] requestMethods = methodMapping.method();
+                if (requestMethods.length == 0) {
+                    requestMethods = RequestMethod.values();
+                }
+                for (RequestMethod requestMethod : requestMethods) {
+                    HandlerKey key = new HandlerKey(fullUrl, requestMethod);
+                    HandlerExecution execution = new HandlerExecution(controllerInstance, method);
+                    handlerExecutions.put(key, execution);
+                }
+            }
+        }
+    }
+
+    private String extractBaseUrl(Class<?> controllerClass) {
+        RequestMapping classMapping = controllerClass.getAnnotation(RequestMapping.class);
+        return (classMapping != null) ? classMapping.value() : "";
     }
 }
