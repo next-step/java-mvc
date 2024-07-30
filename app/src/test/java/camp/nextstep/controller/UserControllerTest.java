1879b33afd0820abb22e2530f5c7b0439bbf570c
HYEJIN <hjwoou@gmail.com>
2024-07-30 22:28:39 +0900

    [docs] objectMapper에 대한 추가 설명 기재

--- app/src/test/java/camp/nextstep/controller/UserControllerTest.java
+++ app/src/test/java/camp/nextstep/controller/UserControllerTest.java
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.Test;
 
 class UserControllerTest {
 
+    /**
+     * objectMapper 기본 설정으로 {@link com.fasterxml.jackson.databind.introspect.VisibilityChecker.Std.DEFAULT} 를 사용하고 있으며,
+     * User는 field가 전부 private하며, setter는 없고, public한 getter가 account만 존재하기 때문에 account만 읽어온다.
+     * 필드 추가가 필요하면 visibility 변경, jackson annotation 활용, getter/setter 추가 등의 다양한 방식을 활용할 수 있다.
+     */
+
     @Test
     void user() {
         var response = RequestUtils.send("/api/user?account=gugu&password=password");
