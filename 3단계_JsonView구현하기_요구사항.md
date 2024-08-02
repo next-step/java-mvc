a404e86e4e2bf58555f3507ef886d2a6c01a6fb6
youngbinary <youngbin@barogo.com>
2024-08-03 00:41:10 +0900

    feat: asis 에 있는 레거시 코드를 삭제한다

--- 3단계_JsonView구현하기_요구사항.md
+++ 3단계_JsonView구현하기_요구사항.md
@@ -45,7 +45,7 @@ public class UserController {
     [x] JSON으로 응답할 때 ContentType은 MediaType.APPLICATION_JSON_UTF8_VALUE으로 반환해야 한다.
 
 [x] app 모듈에 있는 모든 컨트롤러를 어노테이션 기반 MVC 로 변경한다 
-[ ] asis 에 있는 레거시 코드를 삭제한다 
+[x] asis 에 있는 레거시 코드를 삭제한다 
 [ ] Legacy MVC 를 제거한 뒤 DispatcherServlet 을 app 패키지에서 mvc 패키지로 이동한다 
 
 ## 피드백 적용
--- app/src/main/java/camp/nextstep/controller/ForwardController.java
+++ app/src/main/java/camp/nextstep/controller/ForwardController.java
@@ -1,12 +1,10 @@
-package com.interface21.webmvc.servlet.mvc.asis;
+package camp.nextstep.controller;
 
 import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.util.Objects;
 
 @Controller
 public class ForwardController {
