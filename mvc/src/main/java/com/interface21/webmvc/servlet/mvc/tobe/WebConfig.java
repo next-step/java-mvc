865bc6068f00d9543dcc6f73dafb7880abb8c141
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-03 10:26:33 +0900

    refect : 외부에서 의존할 수 있는 부분 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/WebConfig.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/WebConfig.java
@@ -1,29 +1,20 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.mvc.tobe.method.ArgumentResolvers;
+
 public class WebConfig {
 
     private static final String DEFAULT_SUFFIX = ".jsp";
-
     private final HandlerAdapterRegistry handlerAdapterRegistry = new HandlerAdapterRegistry();
     private final ViewResolvers viewResolvers = new ViewResolvers(DEFAULT_SUFFIX);
     private final HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
 
     public void init(String basePackage) {
-        addHandlerAdapter(new AnnotationHandlerAdapter());
-        addViewResolver(new JspViewResolver());
-        addHandlerMapping(new AnnotationHandlerMapping(basePackage));
-    }
-
-    public void addHandlerAdapter(HandlerAdapter handlerAdapter) {
-        handlerAdapterRegistry.addHandlerAdapter(handlerAdapter);
-    }
-
-    public void addViewResolver(ViewResolver viewResolver) {
-        viewResolvers.addViewResolver(viewResolver);
-    }
-
-    public void addHandlerMapping(HandlerMapping handlerMapping) {
-        handlerMappingRegistry.addHandlerMapping(handlerMapping);
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(basePackage);
+        annotationHandlerMapping.initialize();
+        handlerMappingRegistry.addHandlerMapping(annotationHandlerMapping);
+        handlerAdapterRegistry.addHandlerAdapter(new AnnotationHandlerAdapter(ArgumentResolvers.getInstance()));
+        viewResolvers.addViewResolver(new JspViewResolver());
     }
 
     public HandlerAdapterRegistry handlerAdapterRegistry() {
