4cc727537a6893ac8daf477dfe0cb7e74d52d281
gutenLee <3bnm1128@naver.com>
2024-08-05 23:10:51 +0900

    refactor: PathPattern Parse, Util 리팩토링

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/support/PathPatternParser.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/support/PathPatternParser.java
@@ -1,11 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe.support;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import static java.util.function.Predicate.not;
+
+import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class PathPatternParser {
+    private static final String KEY_PATTERN = "(?<%s>[a-zA-z-0-9]+)";
+    private static final String SLASH_PATTERN = "\\/";
+    private static final String WORD_WRAPPED_BRACKET = "\\{[a-zA-z\\-0-9]+\\}";
+    private static final String SLASH = "/";
 
     private final Pattern pattern;
     private final List<String> variableNames = new ArrayList<>();
@@ -15,15 +23,43 @@ public class PathPatternParser {
         this.pattern = Pattern.compile(regex);
     }
 
-    private String buildRegex(String pattern) {
-        return null;
-    }
-
     public boolean matches(String path) {
-        return false;
+        return pattern.matcher(path).find();
     }
 
     public Map<String, String> extractUriVariables(String path) {
-        return null;
+        var matcher = pattern.matcher(path);
+        if (!matcher.find()) {
+            return Map.of();
+        }
+        return variableNames.stream()
+                .map(key -> Map.entry(key, matcher.group(key)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private String buildRegex(String pattern) {
+        return Arrays.stream(pattern.split(SLASH))
+                .filter(not(String::isEmpty))
+                .map(generatePathVariablePattern())
+                .collect(Collectors.joining(SLASH_PATTERN, SLASH_PATTERN, StringUtils.EMPTY));
+    }
+
+    private Function<String, String> generatePathVariablePattern() {
+        return it -> {
+            if (it.matches(WORD_WRAPPED_BRACKET)) {
+                var key = removeBracket(it);
+                addVariableNames(key);
+                return KEY_PATTERN.formatted(key);
+            }
+            return it;
+        };
+    }
+
+    private static String removeBracket(String value) {
+        return value.replaceAll("[{}]", StringUtils.EMPTY);
+    }
+
+    private void addVariableNames(String key) {
+        variableNames.add(key);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/support/PathPatternUtil.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/support/PathPatternUtil.java
@@ -1,83 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe.support;
 
-import static java.util.function.Predicate.not;
-
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.regex.Pattern;
-import java.util.stream.IntStream;
 
 public class PathPatternUtil {
 
-    private static final Pattern BRACKET = Pattern.compile("\\{([a-zA-Z-0-9]+)\\}");
-    private static final String SLASH = "\\/";
-
     public static String getUriValue(String pattern, String path, String key) {
-        assert pattern != null;
-        assert path != null;
-        assert key != null;
-
-        var patterns = split(pattern, SLASH);
-        var values = split(path, SLASH);
-
-        for (int i = 0; i < patterns.length; i++) {
-            if (isMatchedKey(patterns[i], key)) {
-                return values[i];
-            }
-        }
-        return null;
+        return getUriVariables(pattern, path).get(key);
     }
 
     public static Map<String, String> getUriVariables(String pattern, String path) {
-        assert pattern != null;
-        assert path != null;
-        assert PathPatternUtil.isUrlMatch(pattern, path);
-
-        var patterns = split(pattern, SLASH);
-        var paths = split(path, SLASH);
-
-        var map = new HashMap<String, String>();
-        for (int i = 0; i < patterns.length; i++) {
-            var pattern1 = patterns[i];
-            var path1 = paths[i];
-
-            var matcher = BRACKET.matcher(pattern1);
-            if (matcher.find()) {
-                var key = matcher.group(1);
-                map.put(key, getUriValue(pattern1, path1, key));
-            }
-        }
-        return map;
+        var pathPatternParser = new PathPatternParser(pattern);
+        return pathPatternParser.extractUriVariables(path);
     }
 
     public static boolean isUrlMatch(String pattern, String path) {
-
-        var patterns = split(pattern, SLASH);
-        var paths = split(path, SLASH);
-        if (patterns.length != paths.length) {
-            return false;
-        }
-
-        return IntStream.range(0, patterns.length)
-                .allMatch(
-                        i -> {
-                            if (BRACKET.matcher(patterns[i]).find()) {
-                                return true;
-                            }
-                            return Objects.equals(patterns[i], paths[i]);
-                        });
-    }
-
-    private static String[] split(String pattern, String delimiter) {
-        return Arrays.stream(pattern.split(delimiter))
-                .filter(not(String::isEmpty))
-                .toArray(String[]::new);
-    }
-
-    private static boolean isMatchedKey(String pattern, String key) {
-        var matcher = BRACKET.matcher(pattern);
-        return matcher.find() && pattern.equals(pattern.replace(matcher.group(1), key));
+        return new PathPatternParser(pattern).matches(path);
     }
 }
