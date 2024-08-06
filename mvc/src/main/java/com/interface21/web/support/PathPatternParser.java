ef782d26b3873434ae7441f56a58e4c93fd07792
HYEJIN <hjwoou@gmail.com>
2024-08-06 22:03:18 +0900

    [feat] pathPattern util/parser 추가

--- mvc/src/main/java/com/interface21/web/support/PathPatternParser.java
+++ mvc/src/main/java/com/interface21/web/support/PathPatternParser.java
@@ -1,8 +1,6 @@
 package com.interface21.web.support;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class PathPatternParser {
@@ -16,14 +14,29 @@ public class PathPatternParser {
     }
 
     private String buildRegex(String pattern) {
-        return null;
+        var matcher = Pattern.compile("\\{([^}]+)}").matcher(pattern);
+        StringBuffer buffer = new StringBuffer();
+        while (matcher.find()) {
+            variableNames.add(matcher.group(1));
+            matcher.appendReplacement(buffer, "([^/]+)");
+        }
+        matcher.appendTail(buffer);
+        return buffer.toString();
     }
 
     public boolean matches(String path) {
-        return false;
+        return pattern.matcher(path).matches();
     }
 
     public Map<String, String> extractUriVariables(String path) {
-        return null;
+        var matcher = pattern.matcher(path);
+        if (!matcher.matches()) {
+            return Collections.emptyMap();
+        }
+        Map<String, String> variables = new HashMap<>();
+        for (int i = 0; i < variableNames.size(); i++) {
+            variables.put(variableNames.get(i), matcher.group(i + 1));
+        }
+        return variables;
     }
 }
