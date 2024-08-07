2deff50b60a195b7c201988fe356f0f53d3758ac
Jivebreaddev <jivebreaddev@gmail.com>
2024-08-08 08:30:48 +0900

    feature(ModelAndView): ModelAndView render() 메서드를 dispatcherServlet에서 사용할 수 있게끔 변경하였습니다. - Registry를 사용해서 service() 메서드가 동작하게끔 변경하였습니다.

--- app/src/main/java/camp/nextstep/DispatcherServlet.java
+++ app/src/main/java/camp/nextstep/DispatcherServlet.java
@@ -1,52 +1,63 @@
 package camp.nextstep;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMappingRegistry;
+import com.interface21.webmvc.servlet.mvc.tobe.exception.NotFoundException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.webmvc.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
-    private static final String REDIRECT_PREFIX = "redirect:";
 
     private ManualHandlerMapping manualHandlerMapping;
+    private AnnotationHandlerMapping annotationHandlerMapping;
+    private HandlerMappingRegistry mappingRegistry;
+    private HandlerAdapterRegistry adapterRegistry;
 
-    public DispatcherServlet() {
+    public DispatcherServlet(final Object... basePackage) {
+        this.manualHandlerMapping = new ManualHandlerMapping();
+        this.annotationHandlerMapping = new AnnotationHandlerMapping(basePackage);
     }
 
     @Override
     public void init() {
-        this.manualHandlerMapping = new ManualHandlerMapping();
         this.manualHandlerMapping.initialize();
+        this.annotationHandlerMapping.initialize();
+
+        this.mappingRegistry = new HandlerMappingRegistry(
+            List.of(this.manualHandlerMapping, this.annotationHandlerMapping));
+        this.adapterRegistry = new HandlerAdapterRegistry(
+            List.of(new ManualHandlerAdapter(), new AnnotationHandlerAdapter()));
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+    protected void service(final HttpServletRequest request, final HttpServletResponse response)
+        throws ServletException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            final Object handler = mappingRegistry.getHandler(request)
+                .orElseThrow(() -> new NotFoundException("Path Not found"));
+            final HandlerAdapter handlerAdapter = adapterRegistry.getHandlerAdapter(handler)
+                .orElseThrow(() -> new IllegalStateException("Handler Not Found"));
+
+            ModelAndView modelAndView = handlerAdapter.handler(handler, request, response);
+            modelAndView.render(request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
-    }
 }
