b48f8237f93f3e7813cd5ed1bbcded0ec416787b
Jivebreaddev <jivebreaddev@gmail.com>
2024-08-08 08:31:23 +0900

    style(AnnotationHandlerMapping): ControllerScanner로 이름을 변경하였습니다.

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,33 +1,33 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.web.bind.annotation.RequestMethod;
-import com.interface21.webmvc.servlet.mvc.tobe.exception.NotFoundException;
+import com.interface21.webmvc.servlet.mvc.tobe.meta.ControllerScanner;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final HandlerExecutionMeta handlerExecutionMeta;
+    private final ControllerScanner controllerScanner;
     private final Object[] basePackages;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
         this.basePackages = basePackage;
-        this.handlerExecutionMeta = new HandlerExecutionMeta();
+        this.controllerScanner = new ControllerScanner();
     }
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
-        handlerExecutionMeta.initialize(this.basePackages);
+        controllerScanner.initialize(this.basePackages);
     }
 
+    @Override
     public Object getHandler(final HttpServletRequest request) {
         HandlerKey key = HandlerKey.of(request.getRequestURI(),
             RequestMethod.from(request.getMethod()));
 
-        return handlerExecutionMeta.get(key)
-            .orElseThrow(NotFoundException::new);
+        return controllerScanner.get(key);
     }
 }
