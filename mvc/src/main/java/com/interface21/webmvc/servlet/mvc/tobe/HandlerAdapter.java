4210d71a8727f99aa18ba6658f3bb7861954b045
Jivebreaddev <jivebreaddev@gmail.com>
2024-08-07 22:41:04 +0900

    feature(HandlerAdapter, HandlerMapping): 인터페이스 로직 추가합니다.

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapter.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapter.java
@@ -1,5 +1,13 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface HandlerAdapter {
 
+    boolean supports(Object handler);
+
+    ModelAndView handler(Object handler, HttpServletRequest request, HttpServletResponse response)
+        throws Exception;
 }
