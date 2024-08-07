08a23d81c1e9ce993aa972851ad68b4ca03a2752
jeongho <qkr2435@gmail.com>
2024-08-08 01:55:04 +0900

    feat: UserController 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -19,9 +19,8 @@ class JsonViewTest {
     new JsonView().render(Map.of("key", "value"), request, response);
     assertAll(
         () -> assertThat(response.getContentType()).isEqualTo("application/json;charset=UTF-8"),
-        () -> assertThat(response.getContentLength()).isEqualTo(19),
+        () -> assertThat(response.getContentLength()).isEqualTo(15),
         () -> assertThat(response.getContentAsString()).isEqualTo("{\"key\":\"value\"}")
     );
   }
-
 }
