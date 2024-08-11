797ca625f6e95a54e551201af1fbd995608fe5cc
youngbinary <youngbin@barogo.com>
2024-08-12 02:52:34 +0900

    refactor: MethodParameter 사용하지 않는 메서드 정리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/model/MethodParameter.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/model/MethodParameter.java
@@ -1,21 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.model;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Executable;
 import java.lang.reflect.Parameter;
 
 public class MethodParameter {
   private final int index;
   private final Parameter parameter;
-  private final Executable executable;
-  private final Annotation[] parameterAnnotations;
   private final String parameterName;
 
   public MethodParameter(int index, Parameter parameter) {
     this.index = index;
     this.parameter = parameter;
-    this.executable = parameter.getDeclaringExecutable();
-    this.parameterAnnotations = parameter.getAnnotations();
     this.parameterName = parameter.getName();
   }
 
@@ -27,16 +22,8 @@ public class MethodParameter {
     return this.parameter;
   }
 
-  public Executable executable() {
-    return this.executable;
-  }
-
   public String name() {
     return this.parameterName;
-  }
-
-  public Annotation[] parameterAnnotations() {
-    return this.parameterAnnotations;
   }
 
   public Class<?> type() {
